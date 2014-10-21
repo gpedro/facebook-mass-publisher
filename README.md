8d1f50be5591b71360871d2b49135b817d16ddfa
Gabriel Pedro <gpedro831@gmail.com>
2014-10-21 14:35:33 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Facebook Mass Publisher
+[![License](http://img.shields.io/:license-MIT-red.svg)](https://github.com/gpedro/facebook-mass-publisher/blob/master/LICENSE)
+
+# Disclaimer
+Warning: This tool was created just to share knowledge, interesting links and cool events. Do not use for spamming and not for evil. May the force be with you!
+
+# Requirements
+
+1. [Facebook App](https://developers.facebook.com/)
+2, Hosting with HTTPS
+
+# Configuration
+`js/script.js:116`
---
+++ js/script.js
@@ -0,0 +1,120 @@
+$(document).ready(function() {
+
+
+	$('#container').hide();
+	$('#login').click(function() {
+		FB.getLoginStatus(function(response) {
+			if(response.status == 'connected') {
+				FB.logout(function(){
+					$('#name').text('Visitante');
+					$('#login').text('entrar');
+				});
+			} else {
+				FB.login(function(response) {
+					checkLoginState();
+				}, { scope: 'publish_actions,user_groups'});
+			}
+
+			checkLoginState();
+		});
+	});
+
+	$('#send').click(function(e) {
+		e.preventDefault();
+		var body = {};
+		var message = $('#message');
+		var link = $('#link');
+
+		if(message.val().length > 0) {
+			body['message'] = message.val();
+		}
+
+		if(link.val().length > 0) {
+			body['link'] = link.val();
+		}
+
+		$.each($('#groups').tokenfield('getTokens'), function(index, item) {
+			FB.api('/' + item.value + '/feed', 'post', body, function(response) {
+				var result;
+				if (!response || response.error) {
+					result = '<span class="label label-danger">' + response.error.message + ' (Cod ' + response.error.code + ')</span>';
+				} else {
+					result = '<span class="label label-success">' + response.id + '</span>';
+				}
+
+				$('<div class="col-md-10">' + item.label + '</div><div class="col-md-2">' + result + '</div>').appendTo('#queue');
+			});
+		});
+	});
+
+	var checkLoginState = function() {
+		FB.getLoginStatus(function(response) {
+			statusChangeCallback(response);
+		});
+	}
+
+	var getData = function() {
+		FB.api('/me?fields=name,groups{id,name}', function(response) {
+			$('#name').text(response.name);
+			$('#login').text('sair');
+
+			var groups = response.groups.data;
+			var options = [];
+			$.each(groups, function(index, group) {
+				options.push({'value': group.id, 'label': group.name});
+			});
+
+			var engine = new Bloodhound({
+				local: options,
+				datumTokenizer: function(d) {
+					return Bloodhound.tokenizers.whitespace(d.label);
+				},
+				queryTokenizer: Bloodhound.tokenizers.whitespace,
+			});
+
+			engine.initialize();
+
+			$('#groups').tokenfield({
+				typeahead: [null, { source: engine.ttAdapter(), displayKey: 'label' }]
+			});
+
+			$('#groups').on('tokenfield:createtoken', function (event) {
+				var existingTokens = $(this).tokenfield('getTokens');
+				$.each(existingTokens, function(index, token) {
+					if (token.value === event.attrs.value)
+						event.preventDefault();
+				});
+			}).on('tokenfield:createdtoken', function (e) {
+				var valid = false;
+				for(var i = 0; i < options.length; i++) {
+					if (options[i].value == e.attrs.value) {
+						valid = true;
+						break;
+					}
+				}
+
+				if (!valid) {
+					$(e.relatedTarget).addClass('invalid');
+				}
+			});
+		});
+	};
+
+	var statusChangeCallback = function(response) {
+		if(response.status == 'connected') {
+			getData();
+		} else if (response.status == 'not_authorized') {
+			$('#status').text('Falha ao tentar conectar. Acesso não autorizado');
+		} else {
+			$('#status').text('Você não está conectado no Facebook.')
+		}
+	};
+
+	$.ajaxSetup({ cache: true });
+	$.getScript('//connect.facebook.net/pt_BR/all.js', function(){
+		FB.init({
+			appId: 'YOUR_APP_ID_HERE',
+		});
+		FB.getLoginStatus(statusChangeCallback);
+	});
+});
